e520def41a2c1468929f8c640b2195c4b690efc1
Jordão Rodrigues Oliveira Rosario <jordao.rosario01@gmail.com>
2019-02-18 17:25:36 -0300

    Updates

---
+++ precache-manifest.029773f5ab18dc648628123219275eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "068754a15d1e40d667263a06297e96ac",
+    "url": "/static/media/chuck-logo.068754a1.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6216ace10ba2c83a5791",
+    "url": "/static/js/main.6216ace1.chunk.js"
+  },
+  {
+    "revision": "44867a7ac8d3f03c4a63",
+    "url": "/static/js/2.44867a7a.chunk.js"
+  },
+  {
+    "revision": "6216ace10ba2c83a5791",
+    "url": "/static/css/main.39fe9b9d.chunk.css"
+  },
+  {
+    "revision": "de09d94b58669844562fee4ea2c247fa",
+    "url": "/index.html"
+  }
+];
