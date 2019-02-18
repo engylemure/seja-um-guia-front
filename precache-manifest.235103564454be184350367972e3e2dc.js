11139501e02f76e362681935e516fc15d46fd9a8
Jordão Rodrigues Oliveira Rosario <jordao.rosario01@gmail.com>
2019-02-18 19:24:39 -0300

    Updates

---
+++ precache-manifest.235103564454be184350367972e3e2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "068754a15d1e40d667263a06297e96ac",
+    "url": "/seja-um-guia-front/static/media/chuck-logo.068754a1.png"
+  },
+  {
+    "revision": "e225d9555b1f91fd0beb",
+    "url": "/seja-um-guia-front/static/js/runtime~main.e225d955.js"
+  },
+  {
+    "revision": "f400da69eb4a54dfb580",
+    "url": "/seja-um-guia-front/static/js/main.f400da69.chunk.js"
+  },
+  {
+    "revision": "8e5cfbfec38853300d82",
+    "url": "/seja-um-guia-front/static/js/2.8e5cfbfe.chunk.js"
+  },
+  {
+    "revision": "f400da69eb4a54dfb580",
+    "url": "/seja-um-guia-front/static/css/main.9cef4c2f.chunk.css"
+  },
+  {
+    "revision": "fff93d80f32dca8e5112fca8099f4f0d",
+    "url": "/seja-um-guia-front/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/seja-um-guia-front/precache-manifest.a15ea8b077a581c5336e318cbe49c8ff.js"
+  "/seja-um-guia-front/precache-manifest.235103564454be184350367972e3e2dc.js"
 );
 
 workbox.clientsClaim();
