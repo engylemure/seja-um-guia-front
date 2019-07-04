0b0adadac4580f38443fe97121a975b395deffa3
Jordao Rosario <jordao.rosario01@gmail.com>
2019-07-04 12:55:39 -0300

    Updates

---
+++ precache-manifest.5022c00dd3c3fc5a493d20cab72b4932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e243aee8a368f4ffbbcb6e6ae45c3a3",
+    "url": "/seja-um-guia-front/index.html"
+  },
+  {
+    "revision": "f809c3488ec5acbc83ef",
+    "url": "/seja-um-guia-front/static/css/main.e98a4df2.chunk.css"
+  },
+  {
+    "revision": "280bb0b4fae1e065d186",
+    "url": "/seja-um-guia-front/static/js/2.39ac1d5f.chunk.js"
+  },
+  {
+    "revision": "f809c3488ec5acbc83ef",
+    "url": "/seja-um-guia-front/static/js/main.e01797e7.chunk.js"
+  },
+  {
+    "revision": "f2967e91d24a3cdbddbf",
+    "url": "/seja-um-guia-front/static/js/runtime~main.1531f184.js"
+  },
+  {
+    "revision": "068754a15d1e40d667263a06297e96ac",
+    "url": "/seja-um-guia-front/static/media/chuck-logo.068754a1.png"
+  }
+]);
